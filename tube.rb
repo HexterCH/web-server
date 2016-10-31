590dae344b50edd2d971337d02013538a9e2fcf3
Hexter Weng <hexter.ch@gmail.com>
2016-10-31 23:31:30 +0800

    implement app feature

--- tube.rb
+++ tube.rb
@@ -1,21 +1,23 @@
 require "socket"
 require "http/parser"
+require "stringio"
 
 class Tube
-  def initialize(port)
+  def initialize(port, app)
     @server = TCPServer.new(port)
+    @app = app
   end
 
   def start
     loop do
       socket = @server.accept
-      connection = Connection.new(socket)
+      connection = Connection.new(socket, @app)
       connection.process
     end
   end
 
   class Connection
-    def initialize(socket)
+    def initialize(socket, app)
       @socket = socket
       @parser = Http::Parser.new(self)
     end
@@ -32,10 +34,22 @@ class Tube
       puts "  " + @parser.headers.inspect
       puts
 
-      send_response
+      env = {}
+      @parser.headers.each_pair do |name, value|
+        # User-Agent => HTTP_USER_AGENT
+        name = "HTTP_" + name.upcase.tre("-", "_")
+        env[name] = value
+      end
+      env["PATH_INFO"] = @parser.request_url
+      env["REQUEST_METHOD"] = @parser.htpp_method
+      env["rack.input"] = StringIO.new
+
+      send_response env
     end
 
-    def send_response
+    def send_response(env)
+      @app.call(env)
+
       @socket.write "HTTP/1.1 200 OK\r\n"
       @socket.write "\r\n"
       @socket.write "hello\n"
@@ -49,7 +63,20 @@ class Tube
   end
 end
 
-server = Tube.new(3000)
+class App
+  def call(env)
+    message = "Hello from the tube.\n"
+    [
+      200,
+      { 'Content-Type' => 'text/plain', 'Content-Length' => message.size.to_s },
+      [message]
+    ]
+  end
+end
+
+app = App.new
+
+server = Tube.new(3000, app)
 
 puts "plugging tube inot port 3000"
 server.start
